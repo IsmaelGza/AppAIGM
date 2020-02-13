20ede1cd652586eab41bc4af05cc0927a28d6999
IsmaelGza <aigm97111234@gmail.com>
2020-02-13 12:23:49 -0600

    Se agrego una nueva pantalla para ver cambios

---
+++ app/src/main/java/com/example/appaigm/Segundapantalla.kt
@@ -0,0 +1,12 @@
+package com.example.appaigm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Segundapantalla : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segundapantalla)
+    }
+}
